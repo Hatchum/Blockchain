a1c7dee7e7f5d73a7a67694d78d9b906d34d29b8
Arthur Banckaert <arthur.banckaert@gmail.com>
2021-11-24 00:48:59 +0100

    correctif

--- blockchain.js
+++ blockchain.js
@@ -49,7 +49,7 @@ let getGenesisBlock = () => {
     return new Block(0, "0", 1465154705, "Mon genesis block !", "816534932c2b7154836da6afc367695e6337db8a921823784c14378abed4f7d7");
 };
 
-let blockchain = [getGenesisBlock];
+let blockchain = [getGenesisBlock()];
 
 let initHttpServer = () => {
     let app = express();
